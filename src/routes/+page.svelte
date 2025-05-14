d0e7376af6fe0fa7f404681a67e1835a49c719ba
Ajithkumar p <ajith.anvil@gmail.com>
2025-05-14 09:32:51 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-red-300 font-bold bg-cyan-400">Hello</h1>
+<p>Visit <a class="text-blue-400" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
---
+++ src/routes/user/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    function showAlert(){
+        alert("Reju is our big a10thi")
+    }
+</script>
+
+<div>
+    <h1 class="font-bold text-amber-600">Hello!</h1>
+    <button id="button1" onclick={showAlert} class="w-32 p-2  text-white rounded ">
+        click me
+    </button>
+</div>
+
+<style>
+#button1{
+    background-color: blue;
+}
+</style>
